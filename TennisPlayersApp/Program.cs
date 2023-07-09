f06d74754441d90e29da9d24a1c9a6a9cc8346d4
Molee <milosmoskic147@gmail.com>
2023-07-09 13:33:35 +0200

    Configured Porogram.cs class

--- TennisPlayersApp/Program.cs
+++ TennisPlayersApp/Program.cs
@@ -1,3 +1,6 @@
+
+using TennisPlayers.Infastructure.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options =>
+builder.Services.AddDbContext<TennisPlayersContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
