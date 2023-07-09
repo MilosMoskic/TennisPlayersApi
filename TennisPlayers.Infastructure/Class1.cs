6cc5cb40c69015694666ee46fb16a021a9bebf9f
Molee <milosmoskic147@gmail.com>
2023-07-09 13:27:00 +0200

    Created DbContext class and added Many-to-Many relationship between tables

--- TennisPlayers.Infastructure/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TennisPlayers.Infastructure
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ TennisPlayers.Infastructure/Context/TennisPlayersContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TennisPlayers.Domain.Models;
+
+namespace TennisPlayers.Infastructure.Context
+{
+    public class TennisPlayersContext : DbContext
+    {
+        public TennisPlayersContext(DbContextOptions<TennisPlayersContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Athlete> Athletes { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<Tournament> Tournaments { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Sponsor> Sponsors { get; set; }
+        public DbSet<AthleteTournament> AthleteTournaments { get; set; }
+        public DbSet<AthleteSponsor> AthleteSponsors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AthleteTournament>()
+                .HasKey(pc => new { pc.AthleteId, pc.TournamentId });
+            modelBuilder.Entity<AthleteTournament>()
+                .HasOne(a => a.Athlete)
+                .WithMany(pc => pc.AthleteTournaments)
+                .HasForeignKey(a => a.AthleteId);
+            modelBuilder.Entity<AthleteTournament>()
+                .HasOne(t => t.Tournament)
+                .WithMany(pc => pc.AthleteTournaments)
+                .HasForeignKey(t => t.TournamentId);
+
+
+            modelBuilder.Entity<AthleteSponsor>()
+                .HasKey(pc => new { pc.AthleteId, pc.SponsorId });
+            modelBuilder.Entity<AthleteSponsor>()
+                .HasOne(a => a.Athlete)
+                .WithMany(pc => pc.AthleteSponsors)
+                .HasForeignKey(a => a.PokemonId);
+            modelBuilder.Entity<AthleteSponsor>()
+                .HasOne(s => s.Sponsor)
+                .WithMany(pc => pc.AthleteSponsors)
+                .HasForeignKey(s => s.SponsorId);
+        }
+    }
+}
