3ab713d71796294e9090c8c406d057c0694e1e6e
Molee <milosmoskic147@gmail.com>
2023-07-10 14:07:34 +0200

    Created Interface and Repository for Athlete

---
+++ TennisPlayers.Domain/Interfaces/IAthleteRepository.cs
@@ -0,0 +1,13 @@
+﻿using TennisPlayers.Domain.Models;
+
+namespace TennisPlayers.Domain.Interfaces
+{
+    public interface IAthleteRepository
+    {
+        ICollection<Athlete> GetAthletes();
+        Athlete GetAthlete(int id);
+        Athlete GetAthlete(string lastName);
+        Athlete GetAthleteByRanking(int ranking);
+        decimal GetAthleteWinPercent(int id);
+    }
+}
---
+++ TennisPlayers.Infastructure/Repositories/AthleteRepository.cs
@@ -0,0 +1,42 @@
+﻿using TennisPlayers.Domain.Interfaces;
+using TennisPlayers.Domain.Models;
+using TennisPlayers.Infastructure.Context;
+
+namespace TennisPlayers.Infastructure.Repositories
+{
+    public class AthleteRepository : IAthleteRepository
+    {
+        private readonly TennisPlayersContext _context;
+
+        public AthleteRepository(TennisPlayersContext context)
+        {
+            _context = context;
+        }
+        public Athlete GetAthlete(int id)
+        {
+            return _context.Athletes.Where(a => a.Id == id).FirstOrDefault();
+        }
+
+        public Athlete GetAthlete(string lastName)
+        {
+            return _context.Athletes.Where(a => a.LastName == lastName).FirstOrDefault();
+        }
+
+        public Athlete GetAthleteByRanking(int ranking)
+        {
+            return _context.Athletes.Where(a => a.Ranking == ranking).FirstOrDefault();
+        }
+
+        public ICollection<Athlete> GetAthletes()
+        {
+            return _context.Athletes.OrderBy(a => a.LastName).ToList(); 
+        }
+
+        public decimal GetAthleteWinPercent(int id)
+        {
+            var athlete = _context.Athletes.Where(a => a.Id == id).FirstOrDefault();
+
+            return (athlete.TotalWins / (athlete.TotalWins + athlete.TotalLoses)) * 100;
+        }
+    }
+}
