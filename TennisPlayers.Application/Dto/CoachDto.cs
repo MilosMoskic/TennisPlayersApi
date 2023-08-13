d3fbb7248ebba93d1c48835148ca94ab9f9db9e5
Molee <milosmoskic147@gmail.com>
2023-08-13 14:29:18 +0200

    Created Dtos for Coach and Country

---
+++ TennisPlayers.Application/Dto/CoachDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TennisPlayers.Application.Dto
+{
+    public class CoachDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
