1ebacb68eab4fc6a234fa52fefbf77d265b8be2e
Molee <milosmoskic147@gmail.com>
2023-08-13 14:31:57 +0200

    DI for Coach

--- TennisPlayersApp/Program.cs
+++ TennisPlayersApp/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using TennisPlayers.Application.Interfaces;
+using TennisPlayers.Application.Services;
 using TennisPlayers.Domain.Interfaces;
 using TennisPlayers.Infastructure.Context;
 using TennisPlayers.Infastructure.Repositories;
@@ -10,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IAthleteRepository, AthleteRepository>();
+builder.Services.AddScoped<ICoachRepository, CoachRepository>();
+builder.Services.AddScoped<ICoachService, CoachService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
