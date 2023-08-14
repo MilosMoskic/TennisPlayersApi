ddd1c8b828faee656c6c4df348ac12084dd4a2c6
Molee <milosmoskic147@gmail.com>
2023-08-14 22:18:31 +0200

    Athletes Rating set to nullable

---
+++ TennisPlayers.Infastructure/Migrations/20230814194802_set Ranking to null.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TennisPlayers.Infastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class setRankingtonull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Ranking",
+                table: "Athletes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Ranking",
+                table: "Athletes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
