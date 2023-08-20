16fe75abd91d422de1e66cbe07e34a4b4d42ac27
Molee <milosmoskic147@gmail.com>
2023-08-20 16:55:11 +0200

    Implemented validation for inserting data for Location

---
+++ TennisPlayers.Application/Validators/LocationValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System.Text.RegularExpressions;
+using TennisPlayers.Application.Dto;
+
+namespace TennisPlayers.Application.Validators
+{
+    public class LocationValidator : AbstractValidator<LocationDto>
+    {
+        public LocationValidator()
+        {
+            RuleFor(c => c.CityName)
+                .NotNull()
+                .WithMessage("City Name Name cannot be null.")
+                .NotEmpty()
+                .WithMessage("CityName Name cannot be empty.")
+                .Matches(new Regex(@"[A-Za-z]"))
+                .WithMessage("City Name should contain only letters.");
+        }
+    }
+}
